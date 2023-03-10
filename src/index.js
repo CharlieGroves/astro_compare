c37af6395edf942649c28a3a747cbc0e89cf1542
CharlieGroves <62213643+CharlieGroves@users.noreply.github.com>
2023-03-10 15:38:12 +0000

    refactored to remove a file

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import AppWrapper from "./AppWrapper";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import SpaceBackground from "./SpaceBackground";
+import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
 	<React.StrictMode>
+		<SpaceBackground />
 		<Router>
 			<Switch>
-				<Route path='/' component={AppWrapper} />
+				<Route path="/" component={App} />
 			</Switch>
 		</Router>
 	</React.StrictMode>
