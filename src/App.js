73bf61f88325763991fba3a5ea414341c9274039
CharlieGroves <ctrg04@gmail.com>
2023-03-13 21:18:18 +0000

    removed the 1

--- src/App.js
+++ src/App.js
@@ -107,7 +107,7 @@ function App() {
 					<div className="exposure-1">
 						<h3>Exposure 1</h3>
 						<label htmlFor="shutterSpeed_1">
-							Shutter Speed1
+							Shutter Speed
 							<br />
 							<input
 								value={shutterSpeed_1}
