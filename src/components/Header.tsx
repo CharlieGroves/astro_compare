24ffe8759a0da87e1cd7a2edf76d55edb906353f
CharlieGroves <62213643+CharlieGroves@users.noreply.github.com>
2023-04-09 15:40:37 +0100

    better scaling on mobile

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,7 +8,7 @@ import { getAuth } from "firebase/auth";
 
 export default function Header() {
 	const { signOut } = useAuth();
-	const auth = getAuth()
+	const auth = getAuth();
 	const user = auth.currentUser;
 
 	return (
@@ -22,19 +22,20 @@ export default function Header() {
 						<button className="dropbtn">
 							<div className="header-display-name-div">
 								Weclome&nbsp;
-								<span className="header-display-name">
-									{user.displayName}
-								</span>
-								&nbsp;
-								<svg
-									xmlns="http://www.w3.org/2000/svg"
-									viewBox="0 0 320 512"
-									fill="white"
-									height={"20px"}
-								>
-									{/* Font Awesome Pro 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc.*/}
-									<path d="M137.4 374.6c12.5 12.5 32.8 12.5 45.3 0l128-128c9.2-9.2 11.9-22.9 6.9-34.9s-16.6-19.8-29.6-19.8L32 192c-12.9 0-24.6 7.8-29.6 19.8s-2.2 25.7 6.9 34.9l128 128z" />
-								</svg>
+								<div>
+									<span className="header-display-name">
+										{user.displayName}
+									</span>
+									&nbsp;
+									<svg
+										xmlns="http://www.w3.org/2000/svg"
+										viewBox="0 0 320 512"
+										fill="white"
+										height={"20px"}
+									>
+										<path d="M137.4 374.6c12.5 12.5 32.8 12.5 45.3 0l128-128c9.2-9.2 11.9-22.9 6.9-34.9s-16.6-19.8-29.6-19.8L32 192c-12.9 0-24.6 7.8-29.6 19.8s-2.2 25.7 6.9 34.9l128 128z" />
+									</svg>
+								</div>
 							</div>
 						</button>
 						<div className="dropdown-content">
