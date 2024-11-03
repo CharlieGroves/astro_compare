28ffcbfb49fe1198c237ee2e6a25dd5e82a58d18
Charlie Groves <ctrg04@gmail.com>
2024-11-03 11:52:25 -0500

    footer links

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,15 @@ import "../css/Footer.css"
 export default function Footer() {
   return (
     <footer>
-      <p>Created by <a href="https://www.charliegroves.com/">Charlie Groves</a></p>
+      <p>
+        Created by <a href="https://www.charliegroves.com/">Charlie Groves</a> who also made:
+      </p>
+      <ul>
+        <li><a href="https://studycards.ai">StudyCards AI</a></li>
+        <li><a href="https://astrocompare.com">AstroCompare</a></li>
+        <li><a href="https://costofprompts.com">Cost of Prompts</a></li>
+        <li><a href="https://howmuchdoineedtoearn.com">How Much Do I Need to Earn</a></li>
+      </ul>
     </footer>
   );
 }
